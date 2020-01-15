1ea47e78097f5182ba2049961f568a6a493f0c03
linwens <lws200841001@gmail.com>
2020-01-15 22:23:19 +0800

    处理报错

--- examples/components/side-nav.vue
+++ examples/components/side-nav.vue
@@ -52,13 +52,15 @@
 </style>
 <template>
   <div class="side-nav">
-    <div v-for="title in (Object.keys(data))" class="group-container">
+    <div v-for="(title, index) in (Object.keys(data))" class="group-container" :key="index">
       <p class="side-nav-title">{{title}}</p>
-      <div class="side-nav-items" v-for="nav in data[title]" v-if="nav.desc">
-        <router-link :class="$route.name===nav.name ? 'active' : ''" v-if="nav.name" :to="{name: nav.name}">{{nav.desc}}</router-link>
-        <p v-else class="side-nav-group">{{nav.desc}}</p>
-        <div v-for="item in nav.items">
-          <router-link :to="{name: item.name}" :class="$route.name===item.name ? 'active' : ''" class="slid-nav-component">{{item.desc}}</router-link>
+      <div class="side-nav-items" v-for="(nav, index) in data[title]" :key="index">
+        <div v-if="nav.desc">
+          <router-link :class="$route.name===nav.name ? 'active' : ''" v-if="nav.name" :to="{name: nav.name}">{{nav.desc}}</router-link>
+          <p v-else class="side-nav-group">{{nav.desc}}</p>
+          <div v-for="(item, index) in nav.items" :key="index">
+            <router-link :to="{name: item.name}" :class="$route.name===item.name ? 'active' : ''" class="slid-nav-component">{{item.desc}}</router-link>
+          </div>
         </div>
       </div>
     </div>
@@ -66,12 +68,12 @@
 </template>
 
 <script>
-  import navConf from '../nav.config.json'
-  export default {
-    data () {
-      return {
-        data: navConf
-      }
+import navConf from '../nav.config.json'
+export default {
+  data () {
+    return {
+      data: navConf
     }
   }
+}
 </script>
